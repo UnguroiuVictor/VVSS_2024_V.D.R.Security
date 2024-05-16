248384fcc3a824508f2893f4cde0c3aa74c51d48
UnguroiuVictor <92381440+UnguroiuVictor@users.noreply.github.com>
2024-05-16 16:21:01 +0300

    Create module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module pizzashop {
+    requires javafx.graphics;
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires java.desktop;
+
+    opens pizzashop.model to javafx.base;
+    exports pizzashop.model;
+    opens pizzashop to javafx.fxml;
+    exports pizzashop;
+    opens pizzashop.controller to javafx.fxml;
+    exports pizzashop.controller;
+    opens pizzashop.service to javafx.fxml;
+    exports pizzashop.service;
+}
